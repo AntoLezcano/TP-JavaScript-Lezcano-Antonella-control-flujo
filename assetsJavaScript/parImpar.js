9585c0c99832387b9e99fac9c71fb6fbc59831eb
Antonella <antonella.beti@gmail.com>
2024-05-13 11:21:12 -0300

    se creo un estructura de control de flujo en el archivo parImpar

---
+++ assetsJavaScript/parImpar.js
@@ -0,0 +1,13 @@
+// programa que itera del 0 al 10x c/u imprimir si es par-impar
+
+
+for (let i = 0; i <= 10; i++)
+{ 
+    if (i%2 == 0) {
+        console.log(i + " es un número par")
+    }else{
+        console.log(i + " es un número impar")
+    }
+
+}
+
